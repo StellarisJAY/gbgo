50fb35126371940756ccadbba214c9955665746e
xxjay <760409558@qq.com>
2023-09-18 11:23:53 +0800

    feat: 总线访问cartridge

--- bus/bus.go
+++ bus/bus.go
@@ -1,25 +1,46 @@
 package bus
 
+import "github.com/StellarisJAY/gbgo/cartridge"
+
+// Bus 虚拟总线，cpu通过总线地址访问内存和硬件
 type Bus struct {
-	workRAM0 []byte
-	workRAM1 []byte
+	workRAM0  []byte                    // 固定的bank 0
+	workRAM1  []byte                    // cgbMode，可切换的workRam的bank 1~7
+	cartridge *cartridge.BasicCartridge // 卡带数据
 }
 
 func (b *Bus) ReadMem8(addr uint16) byte {
-	// todo read ROM
 	switch {
-	case addr >= 0xC000 && addr <= 0xCFFF:
+	case addr >= 0x0000 && addr <= 0x7FFF: // cartridge banks
+		return b.cartridge.Read(addr)
+	case addr >= 0x8000 && addr <= 0x9FFF: // 显存，可切换bank 0/1
+
+	case addr >= 0xA000 && addr <= 0xBFFF: // 外部RAM
+		return b.cartridge.Read(addr)
+	case addr >= 0xC000 && addr <= 0xCFFF: // work ram bank0
 		return b.workRAM0[addr-0xC000]
-	case addr >= 0xD000 && addr <= 0xDFFF:
+	case addr >= 0xD000 && addr <= 0xDFFF: // work ram bank 1~7
 		return b.workRAM1[addr-0xD000]
 	}
 	return 0
 }
 
 func (b *Bus) WriteMem8(addr uint16, data byte) {
-	// todo bus write
+	switch {
+	case addr >= 0x0000 && addr <= 0x7FFF: // cartridge banks
+		b.cartridge.Write(addr, data)
+	case addr >= 0x8000 && addr <= 0x9FFF: // 显存，可切换bank 0/1
+
+	case addr >= 0xA000 && addr <= 0xBFFF: // 外部RAM
+		b.cartridge.Write(addr, data)
+	case addr >= 0xC000 && addr <= 0xCFFF: // work ram bank0
+		b.workRAM0[addr-0xC000] = data
+	case addr >= 0xD000 && addr <= 0xDFFF: // work ram bank 1~7
+		b.workRAM1[addr-0xD000] = data
+	}
 }
 
 func (b *Bus) ReadMem16(addr uint16) uint16 {
-	return 0
+	low, high := b.ReadMem8(addr), b.ReadMem8(addr+1)
+	return uint16(high)<<8 + uint16(low)
 }
